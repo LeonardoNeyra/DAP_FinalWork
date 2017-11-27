ccc06583220caea7f56d65363407f17d71d8ecdc
Leonardo <lneyrao1@upao.edu.pe>
2017-11-27 00:50:41 -0500

    nunitconf

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CapaNegocio;
+using NUnit.Framework;
 
 namespace UnitTest
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [TestCase]
         public void TestMethod1()
         {
             var c = new NVentanilla();
             string ubicacion = "A17";
             string tipo = "VIP";
-            var exp = c.insertarVentanilla(ubicacion, tipo);
-            Assert.AreEqual(exp, c.insertarVentanilla("A17", "VIP"));
+            var exp = c.insertarVentanilla(ubicacion, tipo); 
+            NUnit.Framework.Assert.AreEqual(exp, c.insertarVentanilla("A17", "VIP"));
         }
     }
 }
